498bd071deeb9a6e0f978e458d111dcace1f4c89
Josué Licona <j.liconam@outlook.com>
2026-07-13 02:19:57 -0500

    Setup ImGui integration with SDL3

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,124 @@
 #define SDL_MAIN_USE_CALLBACKS 1
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <string>
 
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h"
 
 namespace {
 
+constexpr int WindowWidth = 900;
+constexpr int WindowHeight = 640;
+
 struct AppState
 {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
 };
 
-}
+} // namespace
 
 extern "C" {
-
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
-    SDL_SetAppMetadata("Example Renderer Clear", "1.0", "com.example.renderer-clear");
+    const std::string appName = "Game of Life";
+    const std::string appVersion = "0.1.0";
+    const std::string appIdentifier = "com.liconaj.cpp-game-of-life";
+    SDL_SetAppMetadata(appName.c_str(), appVersion.c_str(), appIdentifier.c_str());
 
     *appstate = new AppState();
-    auto* state = static_cast<AppState*>(*appstate);
+    auto& state = *static_cast<AppState*>(*appstate);
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, SDL_WINDOW_RESIZABLE, &state->window, &state->renderer)) {
-        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+    state.window = SDL_CreateWindow(appName.c_str(), WindowWidth, WindowHeight, SDL_WINDOW_RESIZABLE);
+    if (state.window == nullptr) {
+        SDL_Log("Couldn't create window: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    SDL_SetRenderLogicalPresentation(state->renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    state.renderer = SDL_CreateRenderer(state.window, nullptr);
+    if (state.renderer == nullptr) {
+        SDL_Log("Couldn't create renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    // Load font
+    io.FontDefault = io.Fonts->AddFontFromFileTTF("assets/fonts/Inter/Inter_18pt-Regular.ttf", 18.0);
+
+    // Setup Platform / Renderer backends
+
+    if (!ImGui_ImplSDL3_InitForSDLRenderer(state.window, state.renderer)) {
+        SDL_Log("Couldn't initialize imgui");
+        return SDL_APP_FAILURE;
+    }
+    if (!ImGui_ImplSDLRenderer3_Init(state.renderer)) {
+        SDL_Log("Couldn't initialize sdl renderer");
+        return SDL_APP_FAILURE;
+    }
 
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
+    ImGui_ImplSDL3_ProcessEvent(event);
+
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;
     }
+
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
-    AppState& state = *static_cast<AppState*>(appstate);
+    auto& state = *static_cast<AppState*>(appstate);
 
-    const auto now = static_cast<double>(SDL_GetTicks()) / 1000.0;
+    // Star the Dear ImGui frame
+    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+    ImGui::DockSpaceOverViewport();
 
-    const auto red = static_cast<float>(0.5 + 0.5 * SDL_sin(now));
-    const auto green = static_cast<float>(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
-    const auto blue = static_cast<float>(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 2));
+    ImGui::ShowDemoWindow();
 
-    SDL_SetRenderDrawColorFloat(state.renderer, red, green, blue, SDL_ALPHA_OPAQUE_FLOAT);
+    // Rendering
+    SDL_SetRenderDrawColor(state.renderer, 100, 100, 100, 255);
     SDL_RenderClear(state.renderer);
+    ImGui::Render();
+    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), state.renderer);
     SDL_RenderPresent(state.renderer);
+
     return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result)
 {
-    /* SDL will clean up the window/renderer for us. */
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    auto const* state = static_cast<AppState*>(appstate);
+    SDL_DestroyRenderer(state->renderer);
+    SDL_DestroyWindow(state->window);
+    delete state;
+
+    SDL_Quit();
 }
 
 }
